0c76daedb619bdfaf4a3b20f7a1cae943f5e69c4
Borja <boorjabs@gmail.com>
2022-08-23 21:40:21 +0200

    Linting

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -23,7 +23,7 @@ const Header = () => {
                 </div>
             </div>
             <div className="gpt3__header-image">
-                <img src={ai}/>
+                <img src={ai} alt="ai"/>
             </div>
         </div>
     )
